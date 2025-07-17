17fd161bd21b9ee50d2782fb151be6f180622c87
Nguyễn Nam Long <j2teamnnl@gmail.com>
2025-07-17 22:09:02 +0700

    update package

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,18 @@
+// Laravel config JS (axios, lodash, csrf...)
 import './bootstrap';
+
+// jQuery
+import $ from 'jquery';
+window.$ = $;
+window.jQuery = $;
+
+// Bootstrap 5
+import 'bootstrap';
+
+// Select2
+import 'select2';
+import 'select2/dist/css/select2.min.css';
+
+document.addEventListener('DOMContentLoaded', function () {
+    $('.select2').select2();
+});
