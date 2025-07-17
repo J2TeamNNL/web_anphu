03d7900fe06bf9c8cb1abd01f861eb7dc298a41c
Nguyễn Nam Long <j2teamnnl@gmail.com>
2025-07-17 22:09:14 +0700

    Update master.blade.php

--- resources/views/auths/layouts/master.blade.php
+++ resources/views/auths/layouts/master.blade.php
@@ -5,14 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ config('app.name', 'Web Admin') }}</title>
 
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link rel="stylesheet" href="{{ asset('assets/css/style_auth.css') }}">
 
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
-
-    
-    
 </head>
 <body>
     @include('auths.layouts.topBar')
@@ -23,9 +19,6 @@
                 <h4 class="mb-0">
                     @if (request()->routeIs('auths.login'))
                         Đăng nhập CMS
-                    @elseif (request()->routeIs('auths.register'))
-                        Đăng ký tài khoản CMS
-                    @else
                         {{ $title ?? 'Đăng nhập quản lý' }}
                     @endif
                 </h4>
@@ -41,13 +34,9 @@
             </div>
 
             <div class="auth-footer">
-                © {{ now()->year }} AnPhuBuilding
+                ©{{ now()->year }} AnPhuBuilding
             </div>
         </div>
     </div>
-
-    <!-- Bootstrap JS, jQuery -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
