5111fc5c04cc90b76cccb131d0234c45db1841b7
Duc Vu <duc.vh0407@gmail.com>
2025-07-22 12:01:21 +0700

    refactor[UX/UI]: adjust nav_bar size

--- resources/views/customers/partials/nav_bar.blade.php
+++ resources/views/customers/partials/nav_bar.blade.php
@@ -40,86 +40,86 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mx-auto flex-column flex-lg-row">
 
-                <li class="nav-item mx-2 mx-lg-3 my-1 my-lg-0 {{ isActive($homeRoutes) }}">
+                <li class="nav-item mx-2 mx-lg-3 my-1 my-lg-0 small {{ isActive($homeRoutes) }}">
                     <a class="nav-link text-center" href="{{ route('customers.index') }}">
-                        <i class="fa fa-home mr-1"></i><br>Trang Chủ
+                        <i class="fa fa-home mr-1"></i>Trang Chủ
                     </a>
                 </li>
 
-                <li class="nav-item mx-2 mx-lg-3 my-1 my-lg-0 dropdown {{ isActive($aboutRoutes) }}">
+                <li class="nav-item mx-2 mx-lg-3 my-1 my-lg-0 dropdown small {{ isActive($aboutRoutes) }}">
                     <a class="nav-link dropdown-toggle text-center" href="#" id="aboutDropdown" data-toggle="dropdown">
-                        <i class="fa fa-users mr-1"></i><br>Về An Phú
+                        <i class="fa fa-users mr-1"></i>Về An Phú
                     </a>
                     <div class="dropdown-menu">
-                        <a class="dropdown-item {{ isActive('about.anphu') }}" href="{{ route('about.anphu') }}"><i class="fa fa-users icon-highlight"></i> Giới thiệu</a>
-                        <a class="dropdown-item {{ isActive('about.open_letter') }}" href="{{ route('about.open_letter') }}"><i class="fa fa-users icon-highlight"></i> Thư ngỏ</a>
-                        <a class="dropdown-item {{ isActive('about.cultural_values') }}" href="{{ route('about.cultural_values') }}"><i class="fa fa-users icon-highlight"></i> Giá trị văn hóa</a>
+                        <a class="dropdown-item small {{ isActive('about.anphu') }}" href="{{ route('about.anphu') }}"><i class="fa fa-users icon-highlight"></i> Giới thiệu</a>
+                        <a class="dropdown-item small {{ isActive('about.open_letter') }}" href="{{ route('about.open_letter') }}"><i class="fa fa-users icon-highlight"></i> Thư ngỏ</a>
+                        <a class="dropdown-item small {{ isActive('about.cultural_values') }}" href="{{ route('about.cultural_values') }}"><i class="fa fa-users icon-highlight"></i> Giá trị văn hóa</a>
                     </div>
                 </li>
 
-                <li class="nav-item mx-2 mx-lg-3 my-1 my-lg-0 dropdown {{ isActive($serviceRoutes) }}">
+                <li class="nav-item mx-2 mx-lg-3 my-1 my-lg-0 dropdown small {{ isActive($serviceRoutes) }}">
                     <a class="nav-link dropdown-toggle text-center" id="aboutDropdown" data-toggle="dropdown">
-                        <i class="fa fa-cogs mr-1"></i><br>Dịch Vụ
+                        <i class="fa fa-cogs mr-1"></i>Dịch Vụ
                     </a>
                     <div class="dropdown-menu">
-                        <a class="dropdown-item {{ isActive('services.permit') }}" href="{{ route('services.permit') }}"><i class="fa fa-cog mr-1 icon-highlight"></i> Hồ sơ cấp phép xây dựng</a>
-                        <a class="dropdown-item {{ isActive('services.design') }}" href="{{ route('services.design') }}"><i class="fa fa-cog mr-1 icon-highlight"></i> Thiết kế hồ sơ kĩ thuật, nội thất</a>
-                        <a class="dropdown-item {{ isActive('services.construction_full') }}" href="{{ route('services.construction_full') }}"><i class="fa fa-cog mr-1 icon-highlight"></i> Xây nhà trọn gói</a>
-                        <a class="dropdown-item {{ isActive('services.construction_raw') }}" href="{{ route('services.construction_raw') }}"><i class="fa fa-cog mr-1 icon-highlight"></i> Xây dựng phần thô</a>
+                        <a class="dropdown-item small {{ isActive('services.permit') }}" href="{{ route('services.permit') }}"><i class="fa fa-cog mr-1 icon-highlight"></i> Hồ sơ cấp phép xây dựng</a>
+                        <a class="dropdown-item small {{ isActive('services.design') }}" href="{{ route('services.design') }}"><i class="fa fa-cog mr-1 icon-highlight"></i> Thiết kế hồ sơ kĩ thuật, nội thất</a>
+                        <a class="dropdown-item small {{ isActive('services.construction_full') }}" href="{{ route('services.construction_full') }}"><i class="fa fa-cog mr-1 icon-highlight"></i> Xây nhà trọn gói</a>
+                        <a class="dropdown-item small {{ isActive('services.construction_raw') }}" href="{{ route('services.construction_raw') }}"><i class="fa fa-cog mr-1 icon-highlight"></i> Xây dựng phần thô</a>
                     </div>
                 </li>
 
-                <li class="nav-item mx-2 mx-lg-3 my-1 my-lg-0 dropdown {{ in_array($selectedType, ['interior' , 'villa', 'town', 'commercial']) ? 'active' : '' }}">
+                <li class="nav-item mx-2 mx-lg-3 my-1 my-lg-0 dropdown small {{ in_array($selectedType, ['interior' , 'villa', 'town', 'commercial']) ? 'active' : '' }}">
                     <a class="nav-link dropdown-toggle text-center" href="#" id="projectDropdown" data-toggle="dropdown">
-                        <i class="fa fa-cogs mr-1"></i><br>Dự án
+                        <i class="fa fa-cogs mr-1"></i>Dự án
                     </a>
                     <div class="dropdown-menu" aria-labelledby="projectDropdown">
-                        <a class="dropdown-item {{ $selectedType === 'interior' ? 'active' : '' }}"
+                        <a class="dropdown-item small {{ $selectedType === 'interior' ? 'active' : '' }}"
                         href="{{ route('projects.index', ['type' => 'interior']) }}">
-                        <i class="fa fa-cube mr-1 icon-highlight"></i><br>Sản phẩm nội thất
+                        <i class="fa fa-cube mr-1 icon-highlight"></i>Sản phẩm nội thất
                         </a>
-                        <a class="dropdown-item {{ $selectedType === 'villa' ? 'active' : '' }}"
+                        <a class="dropdown-item small {{ $selectedType === 'villa' ? 'active' : '' }}"
                         href="{{ route('projects.index', ['type' => 'villa']) }}">
-                        <i class="fa fa-cube mr-1 icon-highlight"></i><br>Công trình Biệt thự
+                        <i class="fa fa-cube mr-1 icon-highlight"></i>Công trình Biệt thự
                         </a>
-                        <a class="dropdown-item {{ $selectedType === 'town' ? 'active' : '' }}"
+                        <a class="dropdown-item small {{ $selectedType === 'town' ? 'active' : '' }}"
                         href="{{ route('projects.index', ['type' => 'town']) }}">
-                        <i class="fa fa-cube mr-1 icon-highlight"></i><br>Công trình Nhà phố
+                        <i class="fa fa-cube mr-1 icon-highlight"></i>Công trình Nhà phố
                         </a>
-                        <a class="dropdown-item {{ $selectedType === 'commercial' ? 'active' : '' }}"
+                        <a class="dropdown-item small {{ $selectedType === 'commercial' ? 'active' : '' }}"
                         href="{{ route('projects.index', ['type' => 'commercial']) }}">
-                        <i class="fa fa-cube mr-1 icon-highlight"></i><br>Công trình Nhà ở kết hợp kinh doanh
+                        <i class="fa fa-cube mr-1 icon-highlight"></i>Công trình Nhà ở kết hợp kinh doanh
                         </a>
                     </div>
                 </li>
 
-                <li class="nav-item mx-2 mx-lg-3 my-1 my-lg-0 dropdown {{ isActive($priceRoutes) }}">
+                <li class="nav-item mx-2 mx-lg-3 my-1 my-lg-0 dropdown small {{ isActive($priceRoutes) }}">
                     <a class="nav-link dropdown-toggle text-center" href="#" id="aboutDropdown" data-toggle="dropdown">
-                        <i class="fa fa-dollar-sign mr-1"></i><br>Bảng Giá
+                        <i class="fa fa-dollar-sign mr-1"></i>Bảng Giá
                     </a>
                     <div class="dropdown-menu">
-                        <a class="dropdown-item {{ isActive('price.full') }}" href="{{ route('price.full') }}"><i class="fa fa-dollar-sign mr-1 icon-highlight"></i> Báo giá Xây nhà trọn gói</a>
-                        <a class="dropdown-item {{ isActive('price.raw') }}" href="{{ route('price.raw') }}"><i class="fa fa-dollar-sign mr-1 icon-highlight"></i> Báo giá Xây thô</a>
-                        <a class="dropdown-item {{ isActive('price.design') }}" href="{{ route('price.design') }}"><i class="fa fa-dollar-sign mr-1 icon-highlight"></i> Báo giá Thiết kế</a>
-                        <a class="dropdown-item {{ isActive('price.permit') }}" href="{{ route('price.permit') }}"><i class="fa fa-dollar-sign mr-1 icon-highlight"></i> Báo giá Xin cấp phép xây dựng</a>
+                        <a class="dropdown-item small {{ isActive('price.full') }}" href="{{ route('price.full') }}"><i class="fa fa-dollar-sign mr-1 icon-highlight"></i> Báo giá Xây nhà trọn gói</a>
+                        <a class="dropdown-item small {{ isActive('price.raw') }}" href="{{ route('price.raw') }}"><i class="fa fa-dollar-sign mr-1 icon-highlight"></i> Báo giá Xây thô</a>
+                        <a class="dropdown-item small {{ isActive('price.design') }}" href="{{ route('price.design') }}"><i class="fa fa-dollar-sign mr-1 icon-highlight"></i> Báo giá Thiết kế</a>
+                        <a class="dropdown-item small {{ isActive('price.permit') }}" href="{{ route('price.permit') }}"><i class="fa fa-dollar-sign mr-1 icon-highlight"></i> Báo giá Xin cấp phép xây dựng</a>
                     </div>
                 </li>
 
-                <li class="nav-item mx-2 mx-lg-3 my-1 my-lg-0 {{ isActive($consultantRoutes) }}">
+                <li class="nav-item mx-2 mx-lg-3 my-1 my-lg-0 small {{ isActive($consultantRoutes) }}">
                     <a class="nav-link text-center" href="{{ route('customers.consultant') }}">
-                        <i class="fa fa-comments mr-1"></i><br>Tư vấn
+                        <i class="fa fa-comments mr-1"></i>Tư vấn
                     </a>
                 </li>
 
-                <li class="nav-item mx-2 mx-lg-3 my-1 my-lg-0 {{ isActive($blogRoutes) }}">
+                <li class="nav-item mx-2 mx-lg-3 my-1 my-lg-0 small {{ isActive($blogRoutes) }}">
                     <a class="nav-link text-center" href="{{ route('blogs.index') }}">
-                        <i class="fa fa-newspaper mr-1"></i><br>Hoạt động
+                        <i class="fa fa-newspaper mr-1"></i>Hoạt động
                     </a>
                 </li>
 
-                <li class="nav-item mx-2 mx-lg-3 my-1 my-lg-0 {{ isActive($contactRoutes) }}">
+                <li class="nav-item mx-2 mx-lg-3 my-1 my-lg-0 small {{ isActive($contactRoutes) }}">
                     <a class="nav-link text-center" href="{{ route('customers.contact') }}">
-                        <i class="fa fa-phone-alt mr-1"></i><br>Liên hệ
+                        <i class="fa fa-phone-alt mr-1"></i>Liên hệ
                     </a>
                 </li>
             </ul>
