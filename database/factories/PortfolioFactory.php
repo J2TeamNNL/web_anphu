0cfcd5098b2d412acb66a22d744358e154fe0a9f
Duc Vu <duc.vh0407@gmail.com>
2025-07-24 18:13:42 +0700

    delete[PortfolioFactory]

--- database/factories/PortfolioFactory.php
+++ database/factories/PortfolioFactory.php
@@ -22,29 +22,9 @@ class PortfolioFactory extends Factory
 
     public function definition(): array
     {
-        $types = [
-            'villa' => ['modern', 'neoclassic'],
-            'town_house' => ['2story', '3story', '5story', 'singleStory'],
-            'trading_house' => ['appartment', 'office'],
-        ];
-
-        // Random type and matching category
-        $type = $this->faker->randomElement(array_keys($types));
-        $category = $this->faker->randomElement($types[$type]);
 
         return [
-            'name' => $this->faker->sentence(3),
-            'location' => $this->faker->city,
-            'client' => $this->faker->name,
-            'description' => $this->faker->paragraph,
-            'type' => $type,
-            'category' => $category,
-            'year' => $this->faker->numberBetween(2018, now()->year),
-            'image' => 'villa_modenn_1.webp',
-            'image1' => null,
-            'image2' => null,
-            'image3' => null,
-            'image4' => null,
+            //
         ];
     }
 }
