86dc51f481da618d41c5793fd086488092d6af02
Nguyễn Nam Long <j2teamnnl@gmail.com>
2025-08-03 11:02:42 +0700

    fix fillable: Media

--- app/Models/Media.php
+++ app/Models/Media.php
@@ -7,10 +7,18 @@ use Illuminate\Database\Eloquent\Model;
 class Media extends Model
 {
     protected $fillable = [
+        'url',
+        'public_id',
         'file_path',
         'type',
         'caption',
         'order',
+        'mediable_id',
+        'mediable_type'
+    ];
+
+    protected $casts = [
+        'order' => 'integer',
     ];
 
     public function mediable()
