66d17ff299acde4d2e68b6778af59cb396ffdf74
Duc Vu <duc.vh0407@gmail.com>
2025-08-06 22:07:55 +0700

    fix_bug_upload_code_from_local_to_server

--- app/Services/ImageUploadService.php
+++ app/Services/ImageUploadService.php
@@ -30,6 +30,7 @@ class ImageUploadService
                 'image'
             );
 
+            // Log successful upload
             return $media;
 
         } catch (\CloudinaryLabs\CloudinaryLaravel\CloudinaryException $e) {
