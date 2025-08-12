2a3bf07a7b0f2f2d1de4d1a759fe385d0120c08b
Nguyễn Nam Long <j2teamnnl@gmail.com>
2025-08-12 22:21:48 +0700

    Update top_bar.blade.php

--- resources/views/auths/layouts/top_bar.blade.php
+++ resources/views/auths/layouts/top_bar.blade.php
@@ -3,26 +3,14 @@
     <div class="container">
         <div class="row align-items-center">
             <!-- Logo -->
-            <div class="col-12 col-md-6 mb-3 mb-md-0 text-center">
+            <div class="col-12 mb-md-0 text-center">
                 <a href="{{ route('customers.index')}}" class="logo-link d-inline-block">
                     <img
                         class="anphu-logo"
                         src="{{ asset(config('company.assets.logo.main')) }}"
                         alt="{{ config('company.name.brand') }} Logo"
-                        height="50px"
                     >
                 </a>
-            </div>
-
-            <!-- MXH -->
-            <div class="col-12 col-md-6 text-center">
-                <div class="d-flex justify-content-center">
-                    <x-social-media 
-                        size="medium" 
-                        style="default" 
-                        class="d-flex" 
-                    />
-                </div>
             </div>
         </div>
     </div>
