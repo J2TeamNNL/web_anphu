f9a32169920311f408afe12a19da86428f39a098
Nguyễn Nam Long <j2teamnnl@gmail.com>
2025-08-13 22:35:03 +0700

    Update providers.php

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,5 +3,6 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\CloudinaryServiceProvider::class,
+    App\Providers\TelescopeServiceProvider::class,
     App\Providers\ViewServiceProvider::class,
 ];
