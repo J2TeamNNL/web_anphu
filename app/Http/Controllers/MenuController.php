60efc11664b4f858e2ea2f6b4256cfc56a506eb6
Nguyễn Nam Long <j2teamnnl@gmail.com>
2025-08-27 12:23:48 +0700

    Revert "update_ViewServiceProvider"
    This reverts commit 3b890da48715a59b1b1606543d34f8f467c53cad.

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -10,5 +10,26 @@ use Illuminate\Support\Facades\View;
 
 class MenuController extends Controller
 {
-    //
+
+    public static function portfolioNavbarData()
+    {
+        return [
+            'portfoliosCategories' => Category::where('type', CategoryType::PORTFOLIO)
+                ->whereNull('parent_id')
+                ->with('children') 
+                ->get(),
+            'selectedCategory' => null,
+        ];
+    }
+
+    public static function articleNavbarData()
+    {
+        return [
+            'blogsCategories' => Category::where('type', CategoryType::ARTICLE)
+                ->whereNull('parent_id')
+                ->with('children') 
+                ->get(),
+            'selectedCategory' => null,
+        ];
+    }
 }
