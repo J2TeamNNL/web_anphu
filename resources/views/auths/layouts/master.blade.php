12013ab4e7f0b4af691d31b78d580a43bf4b6552
Nguyễn Nam Long <j2teamnnl@gmail.com>
2025-09-14 20:18:34 +0700

    remove unused

--- resources/views/auths/layouts/master.blade.php
+++ resources/views/auths/layouts/master.blade.php
@@ -3,11 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ config('app.name', 'Web Admin') }}</title>
+    <title>AnPhuBuild</title>
     
-    <!-- Favicon -->
-    <link rel="icon" type="image/x-icon" href="{{ asset('assets/img/logo/favicon.ico') }}">
-
     <!-- Bootstrap 4 CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" rel="stylesheet">
     
--- resources/views/customers/layouts/master.blade.php
+++ resources/views/customers/layouts/master.blade.php
@@ -4,8 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <!-- Favicon -->
-    <link rel="icon" type="image/x-icon" href="{{ asset('assets/img/logo/favicon.ico') }}">
     <title>AnPhuBuild</title>
     
     <!-- Bootstrap 4 CSS -->
