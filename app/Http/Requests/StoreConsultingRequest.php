18a9cbf4217ba2f923f08649dfe04260aecdee77
Nguyễn Nam Long <j2teamnnl@gmail.com>
2025-09-20 15:28:11 +0700

    Update StoreConsultingRequest.php

--- app/Http/Requests/StoreConsultingRequest.php
+++ app/Http/Requests/StoreConsultingRequest.php
@@ -3,31 +3,34 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class StoreConsultingRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
     public function authorize(): bool
     {
         return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
     public function rules(): array
     {
         return [
-            'name' => 'nullable|string|max:255',
-            'phone' => 'required|string|max:20',
-            'email' => 'nullable|email|max:255',
-            'location' => 'nullable|string|max:255',
-            'note' => 'nullable|string|max:255',
-            'requirements' => 'nullable|string|max:1000',
+            'phone' => [
+                'nullable',
+                'string',
+                'max:20',
+                Rule::requiredIf(function () {
+                    return $this->email === null;
+                })
+            ],
+            'email' => [
+                'nullable',
+                'email',
+                'max:255',
+                Rule::requiredIf(function () {
+                    return $this->phone === null;
+                })
+            ]
         ];
     }
 }
