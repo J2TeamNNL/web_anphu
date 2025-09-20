baf3c5cdba9c279a7a81f6f739496525162c96bf
Nguyễn Nam Long <j2teamnnl@gmail.com>
2025-09-20 13:53:53 +0700

    Update 2025_09_14_044531_add_address_and_requirements_to_consulting_requests_table.php

--- database/migrations/2025_09_14_044531_add_address_and_requirements_to_consulting_requests_table.php
+++ database/migrations/2025_09_14_044531_add_address_and_requirements_to_consulting_requests_table.php
@@ -24,7 +24,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('consulting_requests', function (Blueprint $table) {
-            $table->dropColumn(['address', 'requirements']);
+            $table->dropColumn(['requirements']);
         });
     }
 };
